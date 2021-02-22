3591eb7d939ebc2e9c69548983d082bf0b7a4e60
Nikita Efremov <heknt90@mail.ru>
2021-02-22 13:08:25 +0300

    changed test_guest_can_go_to_login_page with POM style

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,10 @@
 # For run test type this
 # pytest -v --tb=line --language=en test_main_page.py
 
+from pages.main_page import MainPage
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page
