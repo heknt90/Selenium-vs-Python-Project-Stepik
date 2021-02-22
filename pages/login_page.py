5073b53717a54e859b55972c12c74e593bdf3089
Nikita Efremov <heknt90@mail.ru>
2021-02-22 14:46:44 +0300

    added class LoginPage and tests

---
+++ pages/login_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+link = 'http://selenium1py.pythonanywhere.com/accounts/login/'
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert 'login' in self.browser.current_url, 'Это не страница LoginPage'
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'Форма аутентификации не найдена'
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Форма регистрации нового пользователя не найдена"
+
+    
