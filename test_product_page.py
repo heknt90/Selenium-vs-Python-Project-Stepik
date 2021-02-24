2418290288cb813abae3ba399e2ec6ea3a631343
Nikita Efremov <heknt90@mail.ru>
2021-02-24 21:57:05 +0300

    добавили тест для добавления товара в корзину

---
+++ test_product_page.py
@@ -0,0 +1,14 @@
+from pages.product_page import ProductPage
+from time import sleep
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_add_to_basket_button()
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+    page.check_alerts_after_added_product_to_basket()
+
+
+    
