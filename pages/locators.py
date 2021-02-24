ed0a51a4c00a40b74002d0b9fe64d57aa6609456
Nikita Efremov <heknt90@mail.ru>
2021-02-24 21:57:31 +0300

    добавили локаторы для страницы товара

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,11 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    TITLE = (By.CSS_SELECTOR, "#content_inner h1")
+    PRICE = (By.CSS_SELECTOR, "#content_inner .price_color")
+    SUCCESS_ADD_ALERT = (By.CSS_SELECTOR, "#messages .alert.alert-success:nth-child(1) .alertinner strong")
+    SUTISFIED_OFFER_ALERT = (By.CSS_SELECTOR, "#messages .alert.alert-success:nth-child(2) .alertinner strong")
+    TOTAL_COST_BASKET_ALERT = (By.CSS_SELECTOR, "#messages .alert.alert-info .alertinner p:nth-child(1) strong")
