89e8694978cbdc05c19ff153aeaf251ff919fb2c
Nikita Efremov <heknt90@mail.ru>
2021-03-03 23:36:23 +0300

    добавлены новые методы в BasePage класс и реализованы тесты проверяющие исчезают и появляются ли сообщения о успешном добавлении товара в корзину на странице ProductPage

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,8 @@
-from pages.product_page import ProductPage
 from time import sleep
 import pytest
+
+from pages.product_page import ProductPage
+from pages.locators import ProductPageLocators
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -19,6 +21,35 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
     page.check_alerts_after_added_product_to_basket()
+
+same_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser): 
+    # Открываем страницу товара 
+    page = ProductPage(browser, same_link)
+    page.open()
+    # Добавляем товар в корзину 
+    page.should_be_add_to_basket_button()
+    page.add_product_to_basket()
+    # Проверяем, что нет сообщения об успехе с помощью is_not_element_present
+    assert page.is_not_element_present(*ProductPageLocators.SUCCESS_ADD_ALERT), 'Незарегистрированный пользователь не должен видеть сообщение о добавлении товара в корзину'
+
+def test_guest_cant_see_success_message(browser): 
+    # Открываем страницу товара 
+    page = ProductPage(browser, same_link)
+    page.open()
+    # Проверяем, что нет сообщения об успехе с помощью is_not_element_present
+    assert page.is_not_element_present(*ProductPageLocators.SUCCESS_ADD_ALERT), 'Незарегистрированный пользователь не должен видеть сообщение о добавлении товара в корзину при открытии страницы'
+
+def test_message_disappeared_after_adding_product_to_basket(browser): 
+    # Открываем страницу товара
+    page = ProductPage(browser, same_link)
+    page.open()
+    # Добавляем товар в корзину
+    page.should_be_add_to_basket_button()
+    page.add_product_to_basket()
+    # Проверяем, что нет сообщения об успехе с помощью is_disappeared
+    assert page.is_disappeared(*ProductPageLocators.SUCCESS_ADD_ALERT), "Сообщение о добавлении товара в корзину должно было исчезнуть, но этого не произошло"
 
 
     
