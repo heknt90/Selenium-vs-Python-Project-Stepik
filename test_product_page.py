1966955ab856959f9a65a7167ce3faa4c1941986
Nikita Efremov <heknt90@mail.ru>
2021-03-04 09:08:07 +0300

    пропускаем лишние тесты, добавляем проверку что есть ссылка на страницу входа и что на нее можно перейти

--- test_product_page.py
+++ test_product_page.py
@@ -2,8 +2,12 @@ from time import sleep
 import pytest
 
 from pages.product_page import ProductPage
+from pages.login_page import LoginPage
 from pages.locators import ProductPageLocators
 
+same_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+
+@pytest.mark.skip
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -22,8 +26,22 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.solve_quiz_and_get_code()
     page.check_alerts_after_added_product_to_basket()
 
-same_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    # Проверяем, есть ли ссылка на страницу входа
+    page.should_be_login_link()
 
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    page = ProductPage(browser, same_link)
+    page.open()
+    page.go_to_login_page()
+    # Проверяем, является ли текущая страница страницей входа
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+@pytest.mark.skip
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser): 
     # Открываем страницу товара 
     page = ProductPage(browser, same_link)
@@ -34,6 +52,7 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     # Проверяем, что нет сообщения об успехе с помощью is_not_element_present
     assert page.is_not_element_present(*ProductPageLocators.SUCCESS_ADD_ALERT), 'Незарегистрированный пользователь не должен видеть сообщение о добавлении товара в корзину'
 
+@pytest.mark.skip
 def test_guest_cant_see_success_message(browser): 
     # Открываем страницу товара 
     page = ProductPage(browser, same_link)
@@ -41,6 +60,7 @@ def test_guest_cant_see_success_message(browser):
     # Проверяем, что нет сообщения об успехе с помощью is_not_element_present
     assert page.is_not_element_present(*ProductPageLocators.SUCCESS_ADD_ALERT), 'Незарегистрированный пользователь не должен видеть сообщение о добавлении товара в корзину при открытии страницы'
 
+@pytest.mark.skip
 def test_message_disappeared_after_adding_product_to_basket(browser): 
     # Открываем страницу товара
     page = ProductPage(browser, same_link)
