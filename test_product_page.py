112f6f754cd7fa48c16d385a5a0f5a1b02e5f693
Nikita Efremov <heknt90@mail.ru>
2021-03-04 18:15:23 +0300

    добавляем методы по добавлению товара в корзину для зарегистрированного пользователя

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,4 @@
-from time import sleep
+import time
 import pytest
 
 from pages.product_page import ProductPage
@@ -8,7 +8,7 @@ from pages.locators import ProductPageLocators
 
 same_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 
-@pytest.mark.skip
+
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -27,7 +27,7 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.solve_quiz_and_get_code()
     page.check_alerts_after_added_product_to_basket()
 
-@pytest.mark.skip
+
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -35,7 +35,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     # Проверяем, есть ли ссылка на страницу входа
     page.should_be_login_link()
 
-@pytest.mark.skip
+
 def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, same_link)
     page.open()
@@ -44,7 +44,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
-@pytest.mark.skip
+
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser): 
     # Открываем страницу товара 
     page = ProductPage(browser, same_link)
@@ -55,7 +55,7 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     # Проверяем, что нет сообщения об успехе с помощью is_not_element_present
     assert page.is_not_element_present(*ProductPageLocators.SUCCESS_ADD_ALERT), 'Незарегистрированный пользователь не должен видеть сообщение о добавлении товара в корзину'
 
-@pytest.mark.skip
+
 def test_guest_cant_see_success_message(browser): 
     # Открываем страницу товара 
     page = ProductPage(browser, same_link)
@@ -63,7 +63,7 @@ def test_guest_cant_see_success_message(browser):
     # Проверяем, что нет сообщения об успехе с помощью is_not_element_present
     assert page.is_not_element_present(*ProductPageLocators.SUCCESS_ADD_ALERT), 'Незарегистрированный пользователь не должен видеть сообщение о добавлении товара в корзину при открытии страницы'
 
-@pytest.mark.skip
+
 def test_message_disappeared_after_adding_product_to_basket(browser): 
     # Открываем страницу товара
     page = ProductPage(browser, same_link)
@@ -86,3 +86,28 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page.is_basket_empty()
     # Ожидаем, что есть текст о том что корзина пуста
     basket_page.should_be_no_content_message()
+
+@pytest.mark.register_user
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()
+        email = str(time.time()) + "@fakemail.org"
+        password = str(time.time())
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        page = ProductPage(browser, same_link)
+        page.open()
+        page.should_be_add_to_basket_button()
+        page.add_product_to_basket()
+
+    def test_user_cant_see_success_message(self, browser): 
+        # Открываем страницу товара 
+        page = ProductPage(browser, same_link)
+        page.open()
+        # Проверяем, что нет сообщения об успехе с помощью is_not_element_present
+        assert page.is_not_element_present(*ProductPageLocators.SUCCESS_ADD_ALERT), 'Незарегистрированный пользователь не должен видеть сообщение о добавлении товара в корзину при открытии страницы'
