e5e50d9e0256e34a6ae3563e424958d408d0659a
Nikita Efremov <heknt90@mail.ru>
2021-03-04 11:09:12 +0300

    добавили в тесты для главной страницы и страницы с товароами переход в корзину

--- test_main_page.py
+++ test_main_page.py
@@ -4,8 +4,9 @@
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
 
+link = "http://selenium1py.pythonanywhere.com/"
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
@@ -13,7 +14,19 @@ def test_guest_can_go_to_login_page(browser):
     login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    # Гость открывает главную страницу 
+    page = MainPage(browser, link)
+    page.open()
+    # Переходит в корзину по кнопке в шапке сайта
+    page.should_be_basket_link()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url) 
+    # Ожидаем, что в корзине нет товаров
+    basket_page.is_basket_empty()
+    # Ожидаем, что есть текст о том что корзина пуста
+    basket_page.should_be_no_content_message()
--- test_product_page.py
+++ test_product_page.py
@@ -3,6 +3,7 @@ import pytest
 
 from pages.product_page import ProductPage
 from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
 from pages.locators import ProductPageLocators
 
 same_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
@@ -26,6 +27,7 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.solve_quiz_and_get_code()
     page.check_alerts_after_added_product_to_basket()
 
+@pytest.mark.skip
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -33,6 +35,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     # Проверяем, есть ли ссылка на страницу входа
     page.should_be_login_link()
 
+@pytest.mark.skip
 def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, same_link)
     page.open()
@@ -71,5 +74,15 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     # Проверяем, что нет сообщения об успехе с помощью is_disappeared
     assert page.is_disappeared(*ProductPageLocators.SUCCESS_ADD_ALERT), "Сообщение о добавлении товара в корзину должно было исчезнуть, но этого не произошло"
 
-
-    
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    # Гость открывает страницу товара
+    page = ProductPage(browser, same_link)
+    page.open()
+    # Переходит в корзину по кнопке в шапке 
+    page.should_be_basket_link()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    # Ожидаем, что в корзине нет товаров
+    basket_page.is_basket_empty()
+    # Ожидаем, что есть текст о том что корзина пуста
+    basket_page.should_be_no_content_message()
