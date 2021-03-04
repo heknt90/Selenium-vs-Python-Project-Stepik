2195f3bb60d3270e72413e79bded4eb35fc7ada4
Nikita Efremov <heknt90@mail.ru>
2021-03-04 21:31:53 +0300

    исправлена ошибка в импортах

--- test_main_page.py
+++ test_main_page.py
@@ -3,6 +3,7 @@
 import pytest
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
